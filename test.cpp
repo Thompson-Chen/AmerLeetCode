c443691f321793c2e387d0158063ed4657cab9b0
amer <3272959978@qq.com>
2023-06-24 13:34:31 +0800

    others

--- test.cpp
+++ test.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 int main()
 {
-    cout << ~3<<endl;
-    cout << ~1<<endl;
-    cout << ~2<<endl;
+    int cst = 7;
+
+    for(int i = -2147483648; i <= 2147483647; ++i)
+    {   
+        if(i%10000 == 0)cout<<"@"<<i<<endl;
+        int temp = i^cst;
+        temp = temp^cst;
+        if (temp != i)
+        {
+            cout<<i<<endl;
+            cout<<temp<<endl;
+            return 0;
+        }
+    }
+    cout<<"Finished"<<endl;
     return 0;
 }
-
