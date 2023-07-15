518ffcd5cd1403341081fff3abf91c15d1318f64
耳东 <m15398875935@163.com>
2023-07-15 21:30:22 +0800

    临时存储

---
+++ 496.cpp
@@ -0,0 +1,17 @@
+// 单调栈
+// 下一个更大元素
+// https://leetcode.cn/problems/next-greater-element-i/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// 这道题我感觉不是很简答……
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // 暴力法
+
+        // 单调栈？
+    }
+};
